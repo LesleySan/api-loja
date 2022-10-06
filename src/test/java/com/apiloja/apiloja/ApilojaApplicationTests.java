ca749ce596f3fd39dfda8c09a483e4aeffb5dd7e
LesleySan <morenaroots16@gmail.com>
2022-10-06 20:11:18 -0300

    primeiro commit

---
+++ src/test/java/com/apiloja/apiloja/ApilojaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.apiloja.apiloja;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApilojaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
